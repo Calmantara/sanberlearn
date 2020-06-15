b4865b87008fbb7d7be1f221da55a55595f346f3
calmantara <calmantarasp@gmail.com>
2020-06-15 14:42:57 +0700

    First commit for Week 1 Day 1 Task 1

---
+++ README.md
@@ -0,0 +1,29 @@
+# Code Test Authentication and Fetch
+
+This is code for sanber coding online class.
+
+## Installation
+
+Before you run this project, please kindly install python 3.7.0 or later, and install **jupyter notebook** package.
+
+## Running the tests
+
+To run this code please using jupyternotebook or python compiler
+
+```Bash
+##Week 1 Day 1 task
+cd ./Week1/Day1
+python main.py
+```
+
+Or open the .ipynb files in the corresponding file
+
+## Authors
+
+- **Calmantara Sumpono Putra** - [Github](https://github.com/Calmantara)
+
+## Acknowledgments
+
+- Allah SWT
+- My Mommy
+- All sides that support and inpire me
---
+++ Week1/Day1/main.py
@@ -0,0 +1,37 @@
+# Soal 1
+print("Hello World")
+
+# Soal 2
+print(2.0 + 2.0)
+print(3.0 + 10.0)
+print(100_000.0 - 90_000.0)
+print(399.0 - 199.0)
+print(109.0 * 129.0)
+print(9.0 * 139.0)
+print(109.0 / 129.0)
+print(9.0 / 139.0)
+
+# Soal 3
+num_1 = 200.0
+num_2 = 25.0
+
+int_div = int(num_1 / num_2)
+float_div = num_1 / num_2
+
+print(int_div)
+print(float_div)
+
+# Soal 4
+firstname = "Calmantara"
+lastname = "Sumpono"
+
+print(
+    "Hello sanbercode, saya {} {}! saya siap belajar python data science.".format(
+        firstname, lastname
+    )
+)
+
+# Soal 5
+p = 9.99999
+q = "the number: "
+print("{} {}".format(q, p))
